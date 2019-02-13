a9f605a1e348f86b848ba637a5cd3ee8c6e265b3
Tanate Chumjai <tanate.chu@gmail.com>
2019-02-13 18:22:16 +0700

    [Added] Add background and icon page owner fun Remove

--- app/src/main/java/com/example/thefirstnewprojectaddtoday28jan62/main/owner/PageOwnerFragment.kt
+++ app/src/main/java/com/example/thefirstnewprojectaddtoday28jan62/main/owner/PageOwnerFragment.kt
@@ -2,9 +2,13 @@ package com.example.thefirstnewprojectaddtoday28jan62.main.owner
 
 import android.app.Activity
 import android.app.AlertDialog
+import android.graphics.Canvas
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.helper.ItemTouchHelper
@@ -29,7 +33,11 @@ class PageOwnerFragment : Fragment() {
     var listdata = mutableListOf<Data>()
     lateinit var mActivity: Activity
     lateinit var listMain: RecyclerView
-    //    private var itemremove : String = ""
+
+    private lateinit var deleteIcon: Drawable
+
+
+
     inline fun <reified T> Gson.fromJson(json: String) = this.fromJson<T>(json, object : TypeToken<T>() {}.type)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -43,6 +51,8 @@ class PageOwnerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        deleteIcon = ContextCompat.getDrawable(mActivity, R.drawable.ic_delete)!!
+        var swipeBackground: ColorDrawable = ColorDrawable(ContextCompat.getColor(mActivity, android.R.color.holo_red_dark))
 
         val mRootIns = FirebaseDatabase.getInstance().reference
         mUsersIns = mRootIns.child("PageMain")
@@ -83,27 +93,84 @@ class PageOwnerFragment : Fragment() {
                     adapter.Listdata = listModify
                     adapter.notifyItemRemoved(deleteIndex)
 
-                    Snackbar.make(viewHolder.itemView, " ${oldList[deleteIndex].subject} deleted. ", Snackbar.LENGTH_SHORT)
+                    Snackbar.make(
+                        viewHolder.itemView,
+                        " ${oldList[deleteIndex].subject} deleted. ",
+                        Snackbar.LENGTH_SHORT
+                    )
                         .setAction("UNDO") {
                             listModify.add(deleteIndex, oldList[deleteIndex])
                             adapter.Listdata = listModify
                             adapter.notifyItemInserted(deleteIndex)
-//                            adapter.notifyDataSetChanged()
                         }.show()
                 }
 
+                override fun onChildDraw(
+                    c: Canvas,
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    dX: Float,
+                    dY: Float,
+                    actionState: Int,
+                    isCurrentlyActive: Boolean
+                ) {
+                    val itemView = viewHolder.itemView
+                    val iconMargin = (itemView.height - deleteIcon.intrinsicHeight) / 2
+                    if (dX > 0) {
+                        swipeBackground.setBounds(itemView.left, itemView.top, dX.toInt(), itemView.bottom)
+                        deleteIcon.setBounds(
+                            itemView.left + iconMargin,
+                            itemView.top + iconMargin,
+                            itemView.left + iconMargin + deleteIcon.intrinsicWidth ,
+                            itemView.bottom - iconMargin
+                        )
+                    } else {
+                        swipeBackground.setBounds(
+                            itemView.right + dX.toInt(),
+                            itemView.top,
+                            itemView.right,
+                            itemView.bottom
+                        )
+                        deleteIcon.setBounds(
+                            itemView.right - iconMargin - deleteIcon.intrinsicWidth ,
+                            itemView.top + iconMargin,
+                            itemView.right - iconMargin,
+                            itemView.bottom - iconMargin
+                        )
+                    }
+                    swipeBackground.draw(c)
+                    c.save()
+
+                    if (dX > 0) {
+                        c.clipRect(itemView.left, itemView.top, dX.toInt(), itemView.bottom)
+                    } else {
+                        c.clipRect(
+                            itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom
+                        )
+                    }
+                    deleteIcon.draw(c)
+                    c.restore()
+
+
+
+                    super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                }
+
             }
+
         val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
         itemTouchHelper.attachToRecyclerView(rv_owner)
 
         adapter = OwnerRecyclerAdapter(null)
         listMain.adapter = adapter
+        listMain.setHasFixedSize(true)
         listMain.layoutManager = LinearLayoutManager(mActivity, LinearLayout.VERTICAL, false)
-        adapter.listener = object : OwnerRecyclerAdapter.RecyclerListener {
-            override fun onItemClick(position: Int) {
-                Log.d("Test", "onClickPosition: $position")
+        adapter.listener =
+            object : OwnerRecyclerAdapter.RecyclerListener {
+                override fun onItemClick(position: Int) {
+                    Log.d("Test", "onClickPosition: $position")
+                }
             }
-        }
 
     }
 
