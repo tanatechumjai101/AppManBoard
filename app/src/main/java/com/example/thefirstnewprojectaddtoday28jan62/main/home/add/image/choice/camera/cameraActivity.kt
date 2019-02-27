c639de0fe4a7fe362f865a8d9ccafdcc47c78b4a
Tanate Chumjai <tanate.chu@gmail.com>
2019-02-27 11:25:13 +0700

    [Added] add Gellery

--- app/src/main/java/com/example/thefirstnewprojectaddtoday28jan62/main/home/add/image/choice/camera/cameraActivity.kt
+++ app/src/main/java/com/example/thefirstnewprojectaddtoday28jan62/main/home/add/image/choice/camera/cameraActivity.kt
@@ -18,7 +18,7 @@ import com.example.thefirstnewprojectaddtoday28jan62.main.home.form.FormActivity
 class cameraActivity : AppCompatActivity() {
 
     private val PERMISSION_CODE = 1000
-    lateinit var image_uri : Uri
+    lateinit var image_uri: Uri
     private val IMAGE_CAPTURE_CODE = 1001
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,25 +43,23 @@ class cameraActivity : AppCompatActivity() {
 
     private fun openCamera() {
         val values = ContentValues()
-        values.put(MediaStore.Images.Media.TITLE,"New Picture")
-        values.put(MediaStore.Images.Media.DESCRIPTION,"From the Camera")
-        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values)
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
         val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri)
-        startActivityForResult(cameraIntent,IMAGE_CAPTURE_CODE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
 
-        when(requestCode){
+        when (requestCode) {
             PERMISSION_CODE -> {
 
-                if((grantResults.size) > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if ((grantResults.size) > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     openCamera()
-                }
-                else
-                {
-                    Toast.makeText(this,"Permission denied",Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
                 }
             }
         }
@@ -71,12 +69,12 @@ class cameraActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
 
-        if(resultCode == Activity.RESULT_OK){
-            Log.d("TEST","$image_uri")
-            val intent = Intent(this,FormActivity::class.java)
+        if (resultCode == Activity.RESULT_OK && resultCode == IMAGE_CAPTURE_CODE) {
+//            Log.d("TEST","$image_uri")
+            val intent = Intent(this, FormActivity::class.java)
             startActivity(intent)
-        }else {
-            val intent = Intent(this,FormActivity::class.java)
+        } else {
+            val intent = Intent(this, FormActivity::class.java)
             startActivity(intent)
         }
 
--- app/src/main/java/com/example/thefirstnewprojectaddtoday28jan62/main/home/add/image/choice/gallery/galleryActivity.kt
+++ app/src/main/java/com/example/thefirstnewprojectaddtoday28jan62/main/home/add/image/choice/gallery/galleryActivity.kt
@@ -1,8 +1,15 @@
 package com.example.thefirstnewprojectaddtoday28jan62.main.home.add.image.choice.gallery
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.thefirstnewprojectaddtoday28jan62.R
+import com.example.thefirstnewprojectaddtoday28jan62.main.home.form.FormActivity
 
 class galleryActivity : AppCompatActivity() {
 
@@ -11,7 +18,57 @@ class galleryActivity : AppCompatActivity() {
         setContentView(R.layout.activity_gallery)
 
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                requestPermissions(permissions, PERMISSION_CODE)
+            } else {
+                pickImageFromGallery()
+            }
+        } else {
+            pickImageFromGallery()
+        }
 
 
+    }
+
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            PERMISSION_CODE -> {
+                if ((grantResults.size) > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickImageFromGallery()
+                } else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            val intent = Intent(this, FormActivity::class.java)
+            startActivity(intent)
+        } else {
+            val intent = Intent(this, FormActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    companion object {
+        private val PERMISSION_CODE = 1001
+        private val IMAGE_PICK_CODE = 1000
     }
 }
