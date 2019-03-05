98198c2982aa826ccf1452673e0554e2725fee19
Tanate Chumjai <tanate.chu@gmail.com>
2019-03-05 15:03:26 +0700

    [Added] Add fun camera and Gallery page FromActivity.

--- app/src/main/java/com/example/thefirstnewprojectaddtoday28jan62/main/home/form/FormActivity.kt
+++ app/src/main/java/com/example/thefirstnewprojectaddtoday28jan62/main/home/form/FormActivity.kt
@@ -1,10 +1,12 @@
 package com.example.thefirstnewprojectaddtoday28jan62.main.home.form
 
+import android.Manifest
 import android.app.Activity
 import android.app.AlertDialog
 import android.app.ProgressDialog
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
@@ -25,6 +27,8 @@ import java.text.SimpleDateFormat
 import java.util.*
 import android.os.Environment
 import android.os.StrictMode
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
@@ -41,7 +45,8 @@ class FormActivity : AppCompatActivity() {
     private lateinit var progressDialog: ProgressDialog
 
     private var fileName: String = ""
-
+    private val REQUEST_PERMISSION_CAMERA = 579
+    private val REQUEST_PERMISSION_GALLERY = 975
     var nPreview = ""
     var PreviewHtml = ""
     private val PICK_CAMARA = 1234
@@ -139,11 +144,11 @@ class FormActivity : AppCompatActivity() {
 
             override fun onClick(v: View) {
                 editor.setTextColor(
-                    if (isChanged) {
-                        Color.BLACK
-                    } else {
-                        Color.RED
-                    }
+                        if (isChanged) {
+                            Color.BLACK
+                        } else {
+                            Color.RED
+                        }
                 )
                 isChanged = !isChanged
             }
@@ -154,11 +159,11 @@ class FormActivity : AppCompatActivity() {
 
             override fun onClick(v: View) {
                 editor.setTextBackgroundColor(
-                    if (isChanged) {
-                        Color.TRANSPARENT
-                    } else {
-                        Color.YELLOW
-                    }
+                        if (isChanged) {
+                            Color.TRANSPARENT
+                        } else {
+                            Color.YELLOW
+                        }
                 )
                 isChanged = !isChanged
             }
@@ -200,8 +205,8 @@ class FormActivity : AppCompatActivity() {
 
         action_insert_link.setOnClickListener {
             editor.insertLink(
-                "https://github.com/wasabeef",
-                "career@appman.co.th"
+                    "https://github.com/wasabeef",
+                    "career@appman.co.th"
             )
         }
 
@@ -210,20 +215,30 @@ class FormActivity : AppCompatActivity() {
         }
 
         val dialog = ImageDialog(this)
+
         dialog.listener = object : ImageDialog.DialogListener {
+
             override fun onCameraClick() {
 
-                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, createPathForCameraIntent())
-                startActivityForResult(cameraIntent, PICK_CAMARA)
+
+                if (checkPermission()) {
+                        openCamera()
+                } else {
+                    requestPermission(REQUEST_PERMISSION_CAMERA)
+                }
+
             }
 
             override fun onGalleryClick() {
-                val intent = Intent(Intent.ACTION_PICK)
-                intent.type = "image/*"
-                startActivityForResult(intent, PICK_GALLARY)
+                if (checkPermission()) {
+                    openGallery()
+                } else {
+                    requestPermission(REQUEST_PERMISSION_GALLERY)
+                }
+
             }
         }
+
 
         ib_AddImage.setOnClickListener {
             dialog.show()
@@ -235,11 +250,11 @@ class FormActivity : AppCompatActivity() {
                 finish()
             } else {
                 android.support.v7.app.AlertDialog.Builder(this@FormActivity)
-                    .setTitle("Are you sure ?")
-                    .setMessage("Do you want to close the app?")
-                    .setPositiveButton("yes") { dialog, which -> finish() }
-                    .setNegativeButton("no") { dialog, which -> }
-                    .show()
+                        .setTitle("Are you sure ?")
+                        .setMessage("Do you want to close the app?")
+                        .setPositiveButton("yes") { dialog, which -> finish() }
+                        .setNegativeButton("no") { dialog, which -> }
+                        .show()
             }
         }
 
@@ -260,19 +275,19 @@ class FormActivity : AppCompatActivity() {
             if (ed_subject_from.text.toString().isNullOrBlank() || nPreview.isNullOrBlank()) {
 
                 AlertDialog.Builder(this)
-                    .setIcon(R.drawable.ic_priority_high_black_24dp)
-                    .setTitle("ผิดพลาด")
-                    .setMessage("กรุณากรอกข้อมูลใหม่")
-                    .show()
+                        .setIcon(R.drawable.ic_priority_high_black_24dp)
+                        .setTitle("ผิดพลาด")
+                        .setMessage("กรุณากรอกข้อมูลใหม่")
+                        .show()
             } else {
                 val formPage = Data(
-                    ed_subject_from.text.toString(),
-                    PreviewHtml,
-                    dateTime,
-                    sharedPreference.getString("img_url", ""),
-                    sharedPreference.getString("display_name", ""),
-                    sharedPreference.getString("email", ""),
-                    PrimeryKey_id
+                        ed_subject_from.text.toString(),
+                        PreviewHtml,
+                        dateTime,
+                        sharedPreference.getString("img_url", ""),
+                        sharedPreference.getString("display_name", ""),
+                        sharedPreference.getString("email", ""),
+                        PrimeryKey_id
                 )
                 val intent = Intent().apply {
                     this.putExtra("Data", formPage)
@@ -284,7 +299,42 @@ class FormActivity : AppCompatActivity() {
         }
 
     }
+    private fun openCamera(){
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, createPathForCameraIntent())
+        startActivityForResult(cameraIntent, PICK_CAMARA)
+    }
+    private fun openGallery(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, PICK_GALLARY)
+    }
+    private fun requestPermission(requestCode: Int) {
 
+        ActivityCompat.requestPermissions(
+                this@FormActivity,
+                arrayOf(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ), requestCode
+        )
+
+    }
+
+    fun checkPermission(): Boolean {
+
+        return (ContextCompat.checkSelfPermission(this@FormActivity, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(
+                this@FormActivity,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(
+                this@FormActivity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED)
+
+    }
 
     private fun closeKeyboard() {
         val view = this.currentFocus
@@ -297,11 +347,28 @@ class FormActivity : AppCompatActivity() {
     override fun onBackPressed() {
 //        super.onBackPressed()
         android.support.v7.app.AlertDialog.Builder(this@FormActivity)
-            .setTitle("Are you sure ?")
-            .setMessage("Do you want to close this page?")
-            .setPositiveButton("yes") { dialog, which -> finish() }
-            .setNegativeButton("no") { dialog, which -> }
-            .show()
+                .setTitle("Are you sure ?")
+                .setMessage("Do you want to close this page?")
+                .setPositiveButton("yes") { dialog, which -> finish() }
+                .setNegativeButton("no") { dialog, which -> }
+                .show()
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_PERMISSION_CAMERA -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                        openCamera()
+                }
+            }
+            REQUEST_PERMISSION_GALLERY -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                        openGallery()
+                }
+            }
+        }
     }
 
 
@@ -313,35 +380,35 @@ class FormActivity : AppCompatActivity() {
                     if (imageSavedPath != null) {
                         toast("Bitmap resized.")
                         Glide.with(this)
-                            .asBitmap()
-                            .listener(object : RequestListener<Bitmap?> {
-                                override fun onLoadFailed(
-                                    e: GlideException?,
-                                    model: Any?,
-                                    target: Target<Bitmap?>?,
-                                    isFirstResource: Boolean
-                                ): Boolean {
-                                    return false
-                                }
-
-                                override fun onResourceReady(
-                                    resource: Bitmap?,
-                                    model: Any?,
-                                    target: Target<Bitmap?>?,
-                                    dataSource: DataSource?,
-                                    isFirstResource: Boolean
-                                ): Boolean {
-                                    resource?.let {
-                                        val rescaleBitmap =
-                                            resizeBitmap(resource, resource.width / 2, resource.height / 2)
-                                        uploadImageForCamera(rescaleBitmap.toByteArray())
-
+                                .asBitmap()
+                                .listener(object : RequestListener<Bitmap?> {
+                                    override fun onLoadFailed(
+                                            e: GlideException?,
+                                            model: Any?,
+                                            target: Target<Bitmap?>?,
+                                            isFirstResource: Boolean
+                                    ): Boolean {
+                                        return false
                                     }
-                                    return false
-                                }
-                            })
-                            .load(imageSavedPath)
-                            .into(ivCamera)
+
+                                    override fun onResourceReady(
+                                            resource: Bitmap?,
+                                            model: Any?,
+                                            target: Target<Bitmap?>?,
+                                            dataSource: DataSource?,
+                                            isFirstResource: Boolean
+                                    ): Boolean {
+                                        resource?.let {
+                                            val rescaleBitmap =
+                                                    resizeBitmap(resource, resource.width / 2, resource.height / 2)
+                                            uploadImageForCamera(rescaleBitmap.toByteArray())
+
+                                        }
+                                        return false
+                                    }
+                                })
+                                .load(imageSavedPath)
+                                .into(ivCamera)
                     } else {
                         toast("bitmap not found.")
                     }
@@ -398,22 +465,22 @@ class FormActivity : AppCompatActivity() {
             progressDialog.setTitle("Uploading....")
             val imageRef = storageReference!!.child("Image/Camera/" + UUID.randomUUID().toString())
             imageRef.putBytes(byteArray)
-                .addOnSuccessListener { taskSnapShot ->
-                    progressDialog.dismiss()
-                    Toast.makeText(applicationContext, "Image Uploaded", Toast.LENGTH_SHORT).show()
-                    imageRef.downloadUrl.addOnCompleteListener { p0 ->
-                        editor.insertImage(p0.result.toString(), "Failed")
+                    .addOnSuccessListener { taskSnapShot ->
+                        progressDialog.dismiss()
+                        Toast.makeText(applicationContext, "Image Uploaded", Toast.LENGTH_SHORT).show()
+                        imageRef.downloadUrl.addOnCompleteListener { p0 ->
+                            editor.insertImage(p0.result.toString(), "Failed")
+                        }
                     }
-                }
-                .addOnFailureListener {
-                    progressDialog.dismiss()
-                    Toast.makeText(applicationContext, "Failed", Toast.LENGTH_SHORT).show()
-                }
-                .addOnProgressListener { taskSnapShot ->
-                    val progress = (100.00 * taskSnapShot.bytesTransferred) / taskSnapShot.totalByteCount
-                    progressDialog.setMessage("Uploading  " + progress.toInt() + "  % ...")
+                    .addOnFailureListener {
+                        progressDialog.dismiss()
+                        Toast.makeText(applicationContext, "Failed", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnProgressListener { taskSnapShot ->
+                        val progress = (100.00 * taskSnapShot.bytesTransferred) / taskSnapShot.totalByteCount
+                        progressDialog.setMessage("Uploading  " + progress.toInt() + "  % ...")
 
-                }
+                    }
 
         }
     }
@@ -426,22 +493,22 @@ class FormActivity : AppCompatActivity() {
             progressDialog.setTitle("Uploading....")
             val imageRef = storageReference!!.child("Image/Gallery/" + UUID.randomUUID().toString())
             imageRef.putBytes(byteArray)
-                .addOnSuccessListener { taskSnapShot ->
-                    Toast.makeText(this, "Image Uploaded", Toast.LENGTH_SHORT).show()
-                    progressDialog.dismiss()
-                    imageRef.downloadUrl.addOnCompleteListener { p0 ->
-                        editor.insertImage(p0.result.toString(), "Failed")
+                    .addOnSuccessListener { taskSnapShot ->
+                        Toast.makeText(this, "Image Uploaded", Toast.LENGTH_SHORT).show()
+                        progressDialog.dismiss()
+                        imageRef.downloadUrl.addOnCompleteListener { p0 ->
+                            editor.insertImage(p0.result.toString(), "Failed")
+                        }
                     }
-                }
-                .addOnProgressListener { taskSnapShot ->
-                    var progress = (100.0 * taskSnapShot.bytesTransferred) / taskSnapShot.totalByteCount
-                    progressDialog.setMessage("Uploading " + progress.toInt() + "% ...")
-                }
-                .addOnFailureListener {
-                    progressDialog.dismiss()
-                    Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+                    .addOnProgressListener { taskSnapShot ->
+                        var progress = (100.0 * taskSnapShot.bytesTransferred) / taskSnapShot.totalByteCount
+                        progressDialog.setMessage("Uploading " + progress.toInt() + "% ...")
+                    }
+                    .addOnFailureListener {
+                        progressDialog.dismiss()
+                        Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
 
-                }
+                    }
         }
     }
 
