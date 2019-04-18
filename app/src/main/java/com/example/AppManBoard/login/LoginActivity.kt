d2aedd889fb946a090e3a6688a9bedb35b6af19b
Tanate <tanate.chu.git@gmail.com>
2019-04-18 10:59:39 +0700

    [Added] change service google cosole.

--- app/src/main/java/com/example/AppManBoard/login/LoginActivity.kt
+++ app/src/main/java/com/example/AppManBoard/login/LoginActivity.kt
@@ -16,6 +16,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.activity_login.*
+import com.google.android.gms.common.api.ApiException
+import android.util.Log
 
 
 const val RC_SIGN_IN = 123
@@ -31,11 +33,10 @@ class LoginActivity : AppCompatActivity() {
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .build()
+                .requestEmail()
+                .build()
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
-        btn_signin.visibility = View.VISIBLE
         btn_signin.setSize(SignInButton.SIZE_WIDE)
 
         btn_signin.setOnClickListener {
@@ -46,11 +47,11 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         AlertDialog.Builder(this@LoginActivity)
-            .setTitle("Are you sure ?")
-            .setMessage("Do you want to close the app?")
-            .setPositiveButton("yes") { dialog , which -> finishAffinity() }
-            .setNegativeButton("no") { dialog , which -> }
-            .show()
+                .setTitle("Are you sure ?")
+                .setMessage("Do you want to close the app?")
+                .setPositiveButton("yes") { dialog, which -> finishAffinity() }
+                .setNegativeButton("no") { dialog, which -> }
+                .show()
     }
 
     public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -61,30 +62,37 @@ class LoginActivity : AppCompatActivity() {
                 val task = GoogleSignIn.getSignedInAccountFromIntent(data)
                 handleSignInResult(task)
             } catch (e: Exception) {
-
+                Log.e("error", e.toString())
             }
         }
     }
 
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
 
-        val sharedPreference = getSharedPreferences("SAVE_ACCOUNT", Context.MODE_PRIVATE)
-        val editor = sharedPreference.edit()
-        editor.putString("display_name", completedTask.result.displayName ).apply()
-        editor.putString("email", completedTask.result.email).apply()
-        editor.putString("img_url", completedTask.result.photoUrl.toString()).apply()
 
-        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-        finish()
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            val sharedPreference = getSharedPreferences("SAVE_ACCOUNT", Context.MODE_PRIVATE)
+            val editor = sharedPreference.edit()
+            editor.putString("display_name", account?.displayName).apply()
+            editor.putString("email", account?.email).apply()
+            editor.putString("img_url", account?.photoUrl.toString()).apply()
+            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+            finish()
+
+        } catch (e: ApiException) {
+            Log.e("login", "signInResult:failed code=" + e.statusCode)
+        }
 
 
 //        if (completedTask.result.email!!.contains("@appman.co.th")) {
 //            try {
-//                sign_in_button.visibility = View.GONE
+//
 //            } catch (e: ApiException) {
-//                sign_in_button.visibility = View.GONE
-//            }
+//
+// }
 //            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+//              finish()
 //        } else {
 //            if (mGoogleSignInClient != null) {
 //                mGoogleSignInClient!!.signOut()
